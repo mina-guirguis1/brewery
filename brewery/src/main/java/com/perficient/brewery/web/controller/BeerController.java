d07bbb1377f7ba7c3923ae17ac8b2feae30ea016
mina.guirguis <mina.guirguis@perficient.com>
2022-02-07 17:18:33 -0500

    Added updated beer

--- brewery/src/main/java/com/perficient/brewery/web/controller/BeerController.java
+++ brewery/src/main/java/com/perficient/brewery/web/controller/BeerController.java
@@ -2,12 +2,10 @@ package com.perficient.brewery.web.controller;
 
 import com.perficient.brewery.services.BeerService;
 import com.perficient.brewery.web.model.BeerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -24,5 +22,24 @@ public class BeerController {
     @GetMapping({"/{beerId}"})
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(BeerDto beerDto) {
+        BeerDto saveDto = beerService.savedNewBeer(beerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        // TODO - add hostname url
+        headers.add("Location", "/api/v1/beer" + saveDto.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping({"/{beerId}"})
+    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, BeerDto beerDto) {
+
+        beerService.updateBeer(beerId, beerDto);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
